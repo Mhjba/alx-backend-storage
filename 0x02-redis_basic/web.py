ae671bb37caf3a74a906b9e099d296bf410ad73a
Mhjba <mahjoubaaitalla15@gmail.com>
2024-07-17 18:13:07 +0200

    redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -29,3 +29,7 @@ def data_cacher(method: Callable) -> Callable:
 def get_page(url: str) -> str:
     """ Returns the content of a URL """
     return requests.get(url).text
+
+if __name__ == "__main__":
+    # Example usage
+    test_url = "http://slowwly.robertomurray.co.uk
